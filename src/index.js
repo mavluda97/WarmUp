7ccb914905c3d86c5708d6e1b1e82509db9aa170
mavluda97 <mavluda97@gmail.com>
2019-07-04 23:07:40 +0500

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,10 @@
+module.exports = function warmup(temperature) {
+  // if (temperature === -20) {
+  //   return -4;
+  // }
+  // if (temperature === 0) {
+  //   return 32;
+  // }
+
+  return ((temperature*1.8)+32);
+};
